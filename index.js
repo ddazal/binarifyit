51e4bb3ae23e25279a442a48e9efe75d70b75b4c
David Daza <03dazal@gmail.com>
2018-04-22 19:35:30 -0500

    Initial commit: define and exports main function

---
+++ index.js
@@ -0,0 +1,23 @@
+function testArg (arg, message) {
+  if (typeof arg !== 'number' || arg < 0 || !Number.isInteger(arg)) {
+    throw new Error(message)
+  }
+}
+
+module.exports = function binarify (number, bits = 0) {
+  testArg(number, 'First param must be a positive integer')
+  testArg(bits, 'Second param must be a positive integer')
+
+  let binary = number.toString(2)
+  const size = binary.split('').length
+
+  if (size < bits) {
+    binary = binary.split('')
+    for (let i = 0; i < bits - size; i++) {
+      binary.splice(0, 0, '0')
+    }
+    return binary.join('')
+  }
+
+  return binary
+}
